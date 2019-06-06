d1ae80097d3257ceea05db173fa9e187635fe9c1
Yusuke.Nomura <umaumakkuma@gmail.com>
2019-06-07 00:41:45 +0900

    modified

--- app/main.js
+++ app/main.js
@@ -2,27 +2,49 @@ var app = new Vue({
   el: '#app',
   data: {
     // minNumber: 0,
-    maxNumber: 0,
-    number: [1, 2, 3, 4, 5, 6],
+    maxNumber: 24,
+    // number: [],
+    number: [],
     currentNumber: '',
     numberAllComplete: false,
+    numberList: 0,
     chosenNumber: [{}],
     maxId: 0,
+    setNumberComplete: false,
+    maxNumberValidationError: false,
   },
   methods: {
     setNumber: function() {
-      for (let i = 0; i < this.maxNumber; i++) {
-        this.number[i] = i + 1;
+      this.maxNumber = Number(this.maxNumber);
+      if (this.maxNumber < 1 || this.maxNumber > 101) {
+        this.maxNumberValidationError = true;
+        return;
+      } else {
+        this.maxNumberValidationError = false;
       }
-// console.log(this.number);
+
+      // 番号セット完了
+      this.setNumberComplete = true;
+
+      for (let i = 0; i < this.maxNumber; i++) {
+        this.number.push({
+          id: Number([i]) + 1,
+          class: 'number-' + [i]
+        });
+console.log(this.number);
+      }
+
+      // for (let i = 0; i < this.maxNumber; i++) {
+      //   this.number[i] = i + 1;
+      // }
     },
     stopNumber: function() {
       let random = Math.floor(Math.random() * this.number.length);
       this.currentNumber = this.number[random];
       this.number.splice(random, 1);
-console.log(this.number);
-console.log(random);
-console.log(this.currentNumber);
+// console.log(this.number);
+// console.log(random);
+// console.log(this.currentNumber);
       // 残り玉が 0 の場合
       if (this.number.length <= 0) {
         this.numberAllComplete = true;
