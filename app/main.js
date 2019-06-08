bd8d6868bb3cc5488efb2a9e070ab2c0ba25906f
Yusuke.Nomura <umaumakkuma@gmail.com>
2019-06-09 03:08:32 +0900

    modified

--- app/main.js
+++ app/main.js
@@ -1,21 +1,21 @@
 var app = new Vue({
   el: '#app',
   data: {
-    // minNumber: 0,
-    maxNumber: 24,
-    // number: [],
-    number: [],
-    currentNumber: '',
-    numberAllComplete: false,
-    numberList: 0,
-    chosenNumber: [{}],
-    maxId: 0,
-    setNumberComplete: false,
+    maxNumber: 50,
+    shuffleNumber: 0,
+    chosenNumber: [],
+    remainNumber: [],
     maxNumberValidationError: false,
+    isStartBingo: false,
+    isFinishBingo: false,
+    isShuffle: false
   },
   methods: {
     setNumber: function() {
+      // 最大番号セット
       this.maxNumber = Number(this.maxNumber);
+
+      // 番号バリデーション
       if (this.maxNumber < 1 || this.maxNumber > 101) {
         this.maxNumberValidationError = true;
         return;
@@ -24,45 +24,73 @@ var app = new Vue({
       }
 
       // 番号セット完了
-      this.setNumberComplete = true;
+      this.isStartBingo = true;
 
+      // 番号管理オブジェクト生成
       for (let i = 0; i < this.maxNumber; i++) {
-        this.number.push({
+        this.chosenNumber.push({
           id: Number([i]) + 1,
-          class: 'number-' + [i]
+          isHit: false
         });
-console.log(this.number);
       }
 
-      // for (let i = 0; i < this.maxNumber; i++) {
-      //   this.number[i] = i + 1;
-      // }
+      // ルーレット用番号生成
+      for (let i = 0; i < this.maxNumber; i++) {
+        this.remainNumber[i] = i + 1
+      }
+      this.startShuffle();
     },
-    stopNumber: function() {
-      let random = Math.floor(Math.random() * this.number.length);
-      this.currentNumber = this.number[random];
-      this.number.splice(random, 1);
-// console.log(this.number);
-// console.log(random);
-// console.log(this.currentNumber);
-      // 残り玉が 0 の場合
-      if (this.number.length <= 0) {
-        this.numberAllComplete = true;
+    choiceNumber: function () {
+      // 残り番号からランダムに選択
+      let random = Math.floor(Math.random() * this.remainNumber.length);
+console.log(random);
+      return this.remainNumber[random];
+    },
+    startShuffle: function () {
+      // ビンゴシャッフル表示
+      this.isShuffle = true;
+      shuffleTimer = setInterval(function () {
+        const random = Math.floor(Math.random() * this.remainNumber.length);
+        this.shuffleNumber = this.remainNumber[random];
+      }.bind(this), 20);
+    },
+    stopShuffle: function() {
+      // STOPボタン押下時
+
+      // 選択された番号を取得
+      const random = this.choiceNumber();
+
+      // 出た玉の情報を更新
+      for (let i = 0; i < this.chosenNumber.length; i++) {
+        // 過去に選択されているかチェック
+        if (this.chosenNumber[i].id === random) {
+          if (this.chosenNumber[i].isHit) {
+            // もう一度番号を選択する
+            this.choiceNumber();
+          } else {
+            // 選択済みに更新
+            this.chosenNumber[i].isHit = true;
+          }
+        }
       }
 
-      // if (Object.keys(this.chosenNumber).length > 0) {
-      //   this.maxId = this.chosenNumber.reduce(function (a, b) {
-      //     return a > b.id ? a : b.id
-      //   }, 0);
-      // }
-      // console.log(this.maxId);
+      // シャッフル停止
+      clearInterval(shuffleTimer);
+      this.isShuffle = false;
+      for (let i = 0; i < this.remainNumber.length; i++) {
+        if (this.remainNumber[i] === random) {
+          // 選択されたデカ番号表示
+          this.shuffleNumber = this.remainNumber[i]
+          // 残り番号配列から削除
+          this.remainNumber.splice(i, 1);
+        }
+      }
 
-      this.chosenNumber.push({
-        id: this.maxId + 1,
-        number: this.currentNumber
-      });
-      this.maxId++;
-      // console.log(this.chosenNumber);
+      // 残り玉が 0 の場合
+      if (this.remainNumber.length <= 0) {
+        this.shuffleNumber = 'complete!'
+        this.isFinishBingo = true;
+      }
     }
   }
 })
